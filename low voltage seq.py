d2cbb1569ac9451355a0f95f4b051e2c03483cf2
Mitsuaki Takeuchi <m2takeuchi@gmail.com>
2016-12-19 17:45:48 +0900

    Created No IV measurement version

---
+++ low voltage seq.py
@@ -0,0 +1,30 @@
+### Caution: Python is sensitive for indentation. Do use "Space" insted of "Tab".
+
+dV = 50 # (V) Minimum volgage step, which must be a dvisor for holding voltages.
+
+
+# List of [Voltage (V), holding time(s)]
+DT = 3000 # (s) time per step for time-dependence measurement
+#SEQ = [[5000, 360000]]
+SEQ = [#[2000, DT],\
+        [2000, DT],\
+        [2050, DT],\
+        [2100, DT],\
+        [2150, DT],\
+        [2200, DT],\
+        [2250, DT],\
+        [2300, DT],\
+        [2350, DT],\
+        [2400, DT],\
+        [2450, DT],\
+        [2500, DT],\
+        [2550, DT],\
+        [2600, DT],\
+        [2650, DT],\
+        [2700, DT],\
+        [2750, DT],\
+        [2800, DT],\
+        [2850, DT],\
+        [2900, DT],\
+        [2950, DT],\
+        [3000, 360000]]
